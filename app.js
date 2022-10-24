b14b4a9767410b25fd50199760245136f403511a
Minjun Shin <mjshin97@naver.com>
2022-10-24 16:34:40 +0900

    fifth commit

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-const calculator = {
-    add : function(a,b){
+const calculator = {//+ - / * **
+    add : function(a, b){
         console.log(a, b);
     },
+
+    plus : function(a, b){
+        console.log(a + b);
+    },
+
+    minus : function(a, b){
+        console.log(a - b);
+    },
+
+    devide : function(a, b){
+        console.log(a / b);
+    },
+
+    multiple : function(a, b){
+        console.log(a * b);
+    },
+
+    powerOf : function(a, b){
+        console.log(a ** b);
+    }
 };
 
 calculator.add(5, 1);
+calculator.plus(5, 1);
+calculator.minus(6, 2);
+calculator.devide(6, 2);
+calculator.multiple(6, 2);
+calculator.powerOf(6, 2);
 console.log()
