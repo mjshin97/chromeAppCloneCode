d617eb89ec715d3a3a688f9283145b40de26493d
Minjun Shin <mjshin97@naver.com>
2022-10-24 17:38:23 +0900

    seventh commit

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 const calculator = {//+ - / * **
 
     plus : function(a, b){
-    return a + b;
+        return a + b; //when return some value, function will be finish their job
     },
     
     minus : function(a, b){
-    return a - b;
+        return a - b;
     },
     
     multiple : function(a, b){
-    return a * b;
+        return a * b;
     },
     
     devide : function(a, b){
-    return a / b;
+        return a / b;
     },
     
     powerOf : function(a, b){
-    return a ** b;
+        return a ** b;
     }
 };
 
 const plusResult = calculator.plus(2, 3);
-const minusResult = calculator.minus(plusResult, 10);
-const multipleResult = calculator.multiple(10,minusResult);
-const devideResult = calculator.devide(multipleResult, plusResult);
-const powerOfResult = calculator.powerOf(devideResult, minusResult);
