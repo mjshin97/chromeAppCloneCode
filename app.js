80ddc1dc24d5216f30e869fd3047e81e8db72a58
Minjun Shin <mjshin97@naver.com>
2022-10-26 11:53:28 +0900

    tenth commit

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-const age = parseInt(prompt("How old are you?")); //function is started from the inside to outside
+const title = document.getElementById("title");
 
-if(isNaN(age) || age < 0){
-    console.log("please write a real positive number");
-} else if(age < 18){
-    console.log("you are too young.");
-} else if(age >= 18 && age <= 50){
-    console.log("you can drink.");
-} else if(age > 50 && age <= 80){
-    console.log("you should exercise");
-} else if(age === 100){
-    console.log("wow you are wise");
-} else if(age > 80){
-    console.log("you can do what ever you want.");
-} 
+title.innerText = "Got you!";
+
+console.log(title.id);
+console.log(title.className);
