6eb654af4f17096af19aff1be402b86f784365c8
Minjun Shin <mjshin97@naver.com>
2022-10-27 07:14:06 +0900

    13thcommit

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-const title = document.querySelector("div.hello:first-child h1");
-
-console.dir(title)
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    title.style.color = "blue";
+    const clickedClass = "clicked";
+    if (h1.className === clickedClass) {
+        h1.className = "";
+    } else {
+        h1.className = clickedClass;
+    }
 }
 
-function handleMouseEnter(){
-    title.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave(){
-    title.innerText = "Mouse is gone!";
-}
-
-title.addEventListener("click",handleTitleClick);           //those are the event about mouse condition
-title.addEventListener("mouseenter",handleMouseEnter); //when the mouse is on the word it will be notice you
-title.addEventListener("mouseleave",handleMouseLeave); //when the mouse has been left word will notice you 
+h1.addEventListener("click", handleTitleClick);
