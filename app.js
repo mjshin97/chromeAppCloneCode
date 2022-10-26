d7d6c0e45c3bab0e539fe9e55266e08119f6f6c4
Minjun Shin <mjshin97@naver.com>
2022-10-26 17:16:45 +0900

    12th commit

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const title = document.querySelector("div.hello:first-child h1");
 
+console.dir(title)
+
 function handleTitleClick(){
     title.style.color = "blue";
 }
 
-title.addEventListener("click",handleTitleClick);
-//how to change color by click,how to count how much clicked in console.
+function handleMouseEnter(){
+    title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("click",handleTitleClick);           //those are the event about mouse condition
+title.addEventListener("mouseenter",handleMouseEnter); //when the mouse is on the word it will be notice you
+title.addEventListener("mouseleave",handleMouseLeave); //when the mouse has been left word will notice you 
