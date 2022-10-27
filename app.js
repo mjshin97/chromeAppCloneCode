97fb65032a01be6ba85fe08e85dea239599a8758
Minjun Shin <mjshin97@naver.com>
2022-10-27 20:53:39 +0900

    20th commit

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault();
     console.log(loginInput.value);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
