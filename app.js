e136d9aed1fcb1735b3deade7a85b6089d6e4687
Minjun Shin <mjshin97@naver.com>
2022-10-27 19:04:54 +0900

    17th commit

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick(){
-    h1.classList.toggle("clicked"); //toggles!
+function onLoginBtnClick() {
+    console.log("hello", loginInput.value);
 }
 
-h1.addEventListener("click", handleTitleClick);
-
-
+loginButton.addEventListener("click",onLoginBtnClick);
