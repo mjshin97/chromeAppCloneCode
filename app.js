dc9dd270f30d546c737ef7f2d3c707f9f2dee036
Minjun Shin <mjshin97@naver.com>
2022-10-28 06:54:50 +0900

    21th commit

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username =loginInput.value;
+    greeting.innerText = `Hello ${username} keep going`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
