3058ab1e02c7afa922a08a1d469d91df07f9f1f2
Minjun Shin <mjshin97@naver.com>
2022-10-29 00:17:56 +0900

    27th commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username =loginInput.value;
-    localStorage.setItem(USERNAME_KEY,username); //added
+    localStorage.setItem(USERNAME_KEY,username); 
     paintGreetings(username);
 }
 
