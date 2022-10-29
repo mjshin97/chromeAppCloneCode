161cde6753954b884b41673282d08e6e820b0b0a
Minjun Shin <mjshin97@naver.com>
2022-10-29 13:43:49 +0900

    30th commit

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,7 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`; //has been mistake
 }
 
 getClock(); //Creating a clock
