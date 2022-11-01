70548fc23f66b838baf668eae0b059959624c939
Minjun Shin <mjshin97@naver.com>
2022-11-01 10:01:21 +0900

    39th commit

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+const submitInput = document.querySelector("#submit-form input");
+const submitButton = document.querySelector("#submit-form button");
+
 const playInput = document.querySelector("#play-form input");
 const playButton = document.querySelector("#play-form button");
 
-function onPlayBtnClick() {
-    console.log("hello", playInput.value);
+function onSubmitBtnClick() {
+    if(submitInput.value <= 0){
+        console.log("you should use number over then 0");
+    }
+    else console.log("now you can guess number below", submitInput.value);
 }
 
+function onPlayBtnClick() {
+    if(submitInput.value == playInput.value){
+        console.log("hello", playInput.value);
+    } else {
+        console.log("try again");
+    }
+}
+
+submitButton.addEventListener("click",onSubmitBtnClick);
 playButton.addEventListener("click",onPlayBtnClick);
