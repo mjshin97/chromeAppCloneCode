8e96fd779ad0fcc664e85c71c1eea3f360e1c689
Minjun Shin <mjshin97@naver.com>
2022-11-01 09:32:22 +0900

    38th commit

--- app.js
+++ app.js
@@ -1,28 +1,8 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+const playInput = document.querySelector("#play-form input");
+const playButton = document.querySelector("#play-form button");
 
-const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username";
-
-function onLoginSubmit(event) {
-    event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username =loginInput.value;
-    localStorage.setItem(USERNAME_KEY,username); 
-    paintGreetings(username);
+function onPlayBtnClick() {
+    console.log("hello", playInput.value);
 }
 
-function paintGreetings(username){
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-}
-
-const savedUsername = localStorage.getItem(USERNAME_KEY);
-
-if(savedUsername === null){
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit",onLoginSubmit);
-} else {
-    paintGreetings(savedUsername);
-}
+playButton.addEventListener("click",onPlayBtnClick);
