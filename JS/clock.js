8096f57b0d1a0fb92d20c6c11dab8cab60d9b644
Minjun Shin <mjshin97@naver.com>
2022-11-02 13:41:06 +0900

    43th commit

--- JS/clock.js
+++ JS/clock.js
@@ -1,11 +1,25 @@
 const clock = document.querySelector("h2#clock");
 
+
 function getClock() {
+    /*
     const date = new Date();
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
     clock.innerText = `${date}D ${hours}:${minutes}:${seconds}`; //this is a clock 
+    */
+    
+    
+    const christmas = new Date("2022-12-25:00:00:00+0900");
+    const today = new Date();
+    const gap = christmas.getTime() - today.getTime();
+    const date = Math.ceil(gap / (60 * 60 * 24 * 1000));
+    const hours = Math.floor((gap % (60 * 60 * 24 * 1000)) / (60 * 60 * 1000));
+    const minutes = Math.floor((gap % (60 * 60 * 1000)) / (60 * 1000));
+    const seconds = Math.floor((gap % (60 * 1000)) / 1000);
+    clock.innerText = `${date}D ${hours}:${minutes}:${seconds}`;
+    
 }
 
 getClock();
